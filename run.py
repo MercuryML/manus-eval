035a2c62c9841aeacc10778bb8e178de5fa2c3a5
yumo <yumo@yumo.pub>
2025-09-12 13:45:40 +0800

    eval api

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from api.pg_data import PostgreSQLData
-import re
+import re # noqa
+from tqdm import tqdm
 from typing import List
 
 class EvalOrRL:
     def __init__(self,pg: PostgreSQLData):
         self.pg = pg
+        self.start_date = '2025-09-01'
 
     def load_v2_agents(self):
         sql = 'select id as "agentId","llmId",name as "agentName","updatedAt",responsibilities from "Agents";'  
@@ -38,7 +40,7 @@ class EvalOrRL:
     def load_v2_session(self):
         # select * from "Questions" where "taskId" = 'cmfbeh84500yv18087akmu1z6';
         # select * from "TaskProgresses" where "taskId" = 'cmfbeh84500yv18087akmu1z6';
-        sql = '''select "taskId",index,round,step,content,"updatedAt" from "TaskProgresses" where type='agent:lifecycle:memory:added' and "updatedAt" > '2025-09-01';'''
+        sql = f'''select "taskId",index,round,step,content,"updatedAt" from "TaskProgresses" where type='agent:lifecycle:memory:added' and "updatedAt" > '{self.start_date}';'''
         chat_df = self.pg.read_sql_to_df(sql)
         chat_df = chat_df.sort_values(["taskId","round","index"])
         return chat_df
@@ -47,17 +49,20 @@ class EvalOrRL:
     def eval_v2(self):
         chat_df = self.load_v2_session()
         group_index = ["taskId","round"]
+        bar = tqdm(chat_df.groupby(group_index))
         records = []
-        for (task_id,round),group_df in chat_df.groupby(group_index):
+        for (task_id,round),group_df in bar:
             round = int(round)
             messages = group_df['content'].to_list()
             msg_eval = MessageEval(messages)
             score = msg_eval.compute()
-            print(f"task_id: {task_id}/{round} => {score}")
-            records.append({"taskId":task_id,"round":round,"score":score,"messages":messages})
+            # print(f"task_id: {task_id}/{round} => {score}")
+            bar.set_description(f"task_id: {task_id}/{round} => {score}")
+            # records.append({"taskId":task_id,"round":round,"score":score,"messages":messages})
+            records.append({"taskId":task_id,"round":round,"score":score})
         score_df = pd.DataFrame(records)
         # get agent id
-        sql = '''select "taskId",round,"primaryAgentId" as "agentId" from "Questions" where "isAgentTrigger" is true and "updatedAt" > '2025-09-01';'''
+        sql = f'''select "taskId",round,"primaryAgentId" as "agentId" from "Questions" where "isAgentTrigger" is true and "updatedAt" > '{self.start_date}';'''
         question_df = self.pg.read_sql_to_df(sql)
         score_df = score_df.merge(question_df, on=["taskId","round"], how="left")
         agent_df = self.load_v2_agents()
@@ -68,14 +73,19 @@ class EvalOrRL:
         self.pg.write_df(result_df,"TaskEval",write_mode="overwrite")
 
     def run(self):
+        print('start eval v2')
         result_df = self.eval_v2()
+        print('end eval v2')
+        print('start save result. shape: ', result_df.shape)
         self.save_result(result_df)
+        print('end save result')
 
 class MessageEval:
     "session内评分,支持动态的多rules价值函数"
     def __init__(self,messages: list):
         self.messages = messages    
-        self.score = 0
+        self._score = 0
+        self._score_party_count = 0
         self.llm_messages = self.get_messages(["assistant","tool"])
 
     def __len__(self):
@@ -83,6 +93,10 @@ class MessageEval:
 
     def get_messages(self,roles:List[str]) -> list:
         return [message for message in self.messages if message["role"] in roles]
+    
+    @property
+    def score(self):
+        return round(self._score / self._score_party_count,1)
     
     def compute(self):
         self.has_success_rag()
@@ -96,40 +110,49 @@ class MessageEval:
     def has_success_rag(self) -> bool:
         "具有 rag 调用, 监测通用 rag 调用(function name: rag_query)"
         has_rag = any([msg['message']['name'] == "rag_query" for msg in self.messages if msg['role'] == "tool"])
-        self.score += 1 if has_rag else 0
+        self._score += 1 if has_rag else 0
+        self._score_party_count += 1
         return has_rag
 
     def has_success_tools(self) -> bool:
         "具有工具调用, 监测通用工具调用(包含rag的工具调用)"
         has_tool = any([msg['message']['name'] != "rag_query" for msg in self.messages if msg['role'] == "tool"])
-        self.score += 1 if has_tool else 0
+        self._score += 1 if has_tool else 0
+        self._score_party_count += 1
         return has_tool
     
     def has_success_plan(self):
         """具有计划调用, 监测通用计划调用(function name: plan_query)"""
         has_plan = any([msg['message']['name'] == "plan_query" for msg in self.messages if msg['role'] == "tool"])
-        self.score += 1 if has_plan else 0
+        self._score += 1 if has_plan else 0
+        self._score_party_count += 1
         return has_plan
 
     def has_success_sql(self):
         """具有 sql 调用, 监测通用 sql 调用(function name: sql_query)"""
         has_sql = any([msg['message']['name'] == "sql_query" for msg in self.messages if msg['role'] == "tool"])
-        self.score += 1 if has_sql else 0
+        self._score += 1 if has_sql else 0
+        self._score_party_count += 1
         return has_sql
 
     def has_success_terminal(self):
         """具有 终端 调用, 监测通用终端调用(function name: terminal_query)"""
         has_terminal = any([msg['message']['name'] == "terminal_query" for msg in self.messages if msg['role'] == "tool"])
-        self.score += 1 if has_terminal else 0
+        self._score += 1 if has_terminal else 0
+        self._score_party_count += 1
         return has_terminal
 
     def has_success_html(self):
         """具有 html 调用, 监测通用 html 调用(function name: html_query)"""
         has_html = any([msg['message']['name'] == "html_query" for msg in self.messages if msg['role'] == "tool"])
-        self.score += 1 if has_html else 0
+        self._score += 1 if has_html else 0
+        self._score_party_count += 1
         return has_html
     
 if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+
     pg = PostgreSQLData.load_from_env()
     eval_or_rl = EvalOrRL(pg)
     eval_or_rl.run()
